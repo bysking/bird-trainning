4c095b428ca6b9768792f4e6cfe7feb555456e6a
bysking <bysking@qq.com>
2021-10-15 22:19:17 +0800

    update: 补充缺失的依赖文件

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,20 @@
-import {defineConfig} from 'vite';
-import path from 'path';
-import {createVuePlugin} from 'vite-plugin-vue2';
-import ScriptSetup from 'unplugin-vue2-script-setup/vite'
-import legacy from '@vitejs/plugin-legacy';
+import { defineConfig } from "vite";
+import path from "path";
+import { createVuePlugin } from "vite-plugin-vue2";
+import ScriptSetup from "unplugin-vue2-script-setup/vite";
+import legacy from "@vitejs/plugin-legacy";
 
 export default defineConfig({
   plugins: [
     legacy({
-      targets: ['chrome >= 49', 'safari >= 11.1', 'edge >= 17', 'firefox >= 60', 'ie >= 11', 'not dead'],
+      targets: [
+        "chrome >= 49",
+        "safari >= 11.1",
+        "edge >= 17",
+        "firefox >= 60",
+        "ie >= 11",
+        "not dead",
+      ],
     }),
     createVuePlugin({
       jsx: true,
@@ -19,18 +26,18 @@ export default defineConfig({
     ScriptSetup(),
   ],
   server: {
-    host: '0.0.0.0',
+    host: "0.0.0.0",
     port: 3007,
     // 开启https，会启用http2，但测试后效果一般，没有明显的速度更快
     https: false,
   },
   build: {
-    outDir: path.resolve(__dirname, 'dist'),
+    outDir: path.resolve(__dirname, "dist"),
     emptyOutDir: true,
     minify: false,
   },
   define: {
-    'process.env': {
+    "process.env": {
       NODE_ENV: `"${process.env.NODE_ENV}"`,
       localMode: false,
     },
@@ -52,5 +59,5 @@ export default defineConfig({
   //         },
   //     },
   // },
-  logLevel: 'info',
+  logLevel: "info",
 });

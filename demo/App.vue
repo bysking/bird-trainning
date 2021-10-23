222e25a958f6da6708e5ecacb08286e28c6dd037
bysking <bysking@qq.com>
2021-10-23 17:48:35 +0800

    update: 丰羽计划第一期

--- demo/App.vue
+++ demo/App.vue
@@ -1,24 +1,126 @@
 <template>
   <div>
-    <TestTable :test="test">
-      <span>xxxxxx</span>
-    </TestTable>
+    <button @click="testLoad">测试加载数据</button>
+    <button @click="testSort">测试数据排序</button>
+    <button @click="testClearSort">测试清除数据</button>
+    <button @click="testJumpNext">测试跳转下一页</button>
+    <TestPage :tableConfig="tableConfig" :list="list" />
   </div>
 </template>
 
 <script lang="ts">
-import { TestTable } from '../src/table'
-import { defineComponent, ref } from '@vue/composition-api'
+import TestPage from "../src/table/TablePage";
+import { defineComponent, ref } from "@vue/composition-api";
+import type { Ref } from "@vue/composition-api";
 
 export default defineComponent({
-  name: 'App',
+  name: "App",
   components: {
-    TestTable,
+    TestPage,
   },
   setup() {
-    const test = ref(true)
+    const refTable: Ref<any> = ref(null);
+    const tableConfig = ref({
+      columns: [
+        {
+          title: "姓名",
+          key: "name",
+        },
+        {
+          title: "年龄",
+          key: "age",
+          sort: true,
+          defaultSort: "ASC",
+        },
+        {
+          title: "地址",
+          key: "address",
+        },
+      ],
+    });
 
-    return { test }
+    const list = ref([
+      {
+        name: "John Brown",
+        age: 18,
+        address: "New York No. 1 Lake Park",
+        date: "2016-10-03",
+      },
+      {
+        name: "Jim Green",
+        age: 24,
+        address: "London No. 1 Lake Park",
+        date: "2016-10-01",
+      },
+      {
+        name: "Joe Black",
+        age: 30,
+        address: "Sydney No. 1 Lake Park",
+        date: "2016-10-02",
+      },
+      {
+        name: "Jon Snow",
+        age: 26,
+        address: "Ottawa No. 2 Lake Park",
+        date: "2016-10-04",
+      },
+      {
+        name: "Jon Snow1",
+        age: 27,
+        address: "Ottawa No. 2 Lake Park",
+        date: "2016-10-04",
+      },
+      {
+        name: "Jon Snow2",
+        age: 28,
+        address: "Ottawa No. 2 Lake Park",
+        date: "2016-10-04",
+      },
+      {
+        name: "Jon Snow3",
+        age: 29,
+        address: "Ottawa No. 2 Lake Park",
+        date: "2016-10-04",
+      },
+      {
+        name: "Jon Snow4",
+        age: 76,
+        address: "Ottawa No. 2 Lake Park",
+        date: "2016-10-04",
+      },
+      {
+        name: "Jon Snow5",
+        age: 86,
+        address: "Ottawa No. 2 Lake Park",
+        date: "2016-10-04",
+      },
+    ]);
+
+    const testLoad = () => {
+      refTable.value.loadData([]);
+    };
+
+    const testSort = () => {
+      refTable.value.setTableSort([]);
+    };
+
+    const testClearSort = () => {
+      refTable.value.clearTableSort();
+    };
+
+    const testJumpNext = () => {
+      refTable.value.pageMoveNext();
+    };
+
+    return {
+      refTable,
+      testLoad,
+      testSort,
+      tableConfig,
+      list,
+      testClearSort,
+      testJumpNext,
+    };
   },
-})
+});
 </script>
--- src/__tests__/table.spec.js
+++ src/__tests__/table.spec.js
@@ -1,4 +1,5 @@
 import { mount } from "@vue/test-utils";
+// import { shallowMount } from '@vue/test-utils';
 import { TestTable } from "../table";
 
 describe("Table", () => {
@@ -17,4 +18,31 @@ describe("Table", () => {
     const wrapper = mount(TestTable);
     expect(wrapper.contains("table-hd-tr-th")).toBe(false);
   });
+});
+
+test("测试表格的loadData方法能不能正常加载数据", () => {
+  // const wrapper = shallowMount(TestTable);
+  // let list = [{ name: '111', age: 12}, { name: '222', age: 222 }]
+  // wrapper.vm.loadData(list);
+  // 表格已经成功渲染，检测表格实例的渲染数据有值，并且和list内容相等
+});
+
+test("测试表格的排序", () => {
+  // const wrapper = shallowMount(TestTable);
+  // let list = [{ name: '111', age: 12}, { name: '222', age: 222 }]
+  // wrapper.vm.loadData(list);
+  // wrapper.vm.setTableSort({ name: 'ASC' })
+  // wrapper.vm.排序属性对象为 { name: '' }
+  // 表格已经成功排序，表格name对应的列表现为升序高亮，展示内容暗战升序排序
+});
+
+test("测试表格的清除排序", () => {
+  // const wrapper = shallowMount(TestTable);
+  // let list = [{ name: '111', age: 12}, { name: '222', age: 222 }]
+  // wrapper.vm.loadData(list);
+  // wrapper.vm.clearTableSort()
+  // wrapper.vm.setTableSort({ name: 'ASC' })
+  // wrapper.vm.clearTableSort()
+  // wrapper.vm.排序属性对象为 { name: '' }
+  // 表格已经清除排序，表格name对应的列表现为升序默认的顺序
 });
